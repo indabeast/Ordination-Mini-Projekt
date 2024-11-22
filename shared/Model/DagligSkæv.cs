978b3bb088bab8c1a41af36d48c2319f5abdb655
hendrikmolgaard <Moelgaard01@hotmail.com>
2024-11-22 08:57:15 +0100

    Initial commit

---
+++ shared/Model/DagligSkæv.cs
@@ -0,0 +1,39 @@
+namespace shared.Model;
+
+public class DagligSkæv : Ordination {
+    public List<Dosis> doser { get; set; } = new List<Dosis>();
+
+    public DagligSkæv(DateTime startDen, DateTime slutDen, Laegemiddel laegemiddel) : base(laegemiddel, startDen, slutDen) {
+	}
+
+    public DagligSkæv(DateTime startDen, DateTime slutDen, Laegemiddel laegemiddel, Dosis[] doser) : base(laegemiddel, startDen, slutDen) {
+        this.doser = doser.ToList();
+    }    
+
+    public DagligSkæv() : base(null!, new DateTime(), new DateTime()) {
+    }
+
+	public void opretDosis(DateTime tid, double antal) {
+        doser.Add(new Dosis(tid, antal));
+    }
+
+	public override double samletDosis() {
+		return base.antalDage() * doegnDosis();
+	}
+
+	
+	public override double doegnDosis()
+	{
+		double doegnDosis = 0;
+		foreach (Dosis d in doser)
+		{
+			doegnDosis += d.antal;
+		}
+		return doegnDosis;
+		
+	}
+
+	public override String getType() {
+		return "DagligSkæv";
+	}
+}
